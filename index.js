57153453dee6a6c87cb45ef664ddb36ca581e000
Ngaremaina <ngaremaina4@gmail.com>
2023-03-13 12:06:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ let welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
 // let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
 let excitedWelcomeMessage = welcomeMessage.toUpperCase();
 let shortGreeting = `Welcome, ${currentUser[0]} !`;
+// let shortGreeting = "Welcome, " + currentUser.slice(0, 1) + "!";
 
